e4b879d907a430348af46ed6ae9439a82a5917f9
Duke-wei <13540639584@163.com>
2017-04-18 11:14:01 +0800

    fix problem with static function can not modify by const

--- stringtool.h
+++ stringtool.h
@@ -11,13 +11,14 @@
 #include<string>
 #include<cstring>
 #include<cstdlib>
+#include<vector>
 
 namespace basetool{
 	class StringTool{
 		public:
 			static std::vector<std::string> splitstring(const std::string& str,const char delimiter=' ');
-			static std::string chartostring(char *src) const;
-			static const char* stringtochar(std::string src) const;
+			static std::string chartostring(char *src);
+			static const char* stringtochar(std::string src);
 			static int stringKMP(std::string src,std::string part);
 			static bool isomorphic(std::string src,std::string src2);
 			static bool ispalindromic(std::string src);
